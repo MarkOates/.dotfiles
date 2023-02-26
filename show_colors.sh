62db2d0a05d6e839df41f3d4e0a0cde2d72ef568
Mark Oates <markoates0@gmail.com>
2023-02-25 23:58:50 -0500

    Update statusbar to look more punctual

--- show_colors.sh
+++ show_colors.sh
@@ -5,3 +5,9 @@ for((i=16; i<256; i++)); do
     [ ! $((($i - 15) % 6)) -eq 0 ] && printf ' ' || printf '\n'
 done
 
+for((i=16; i<256; i++)); do
+    printf "\e[38;5;${i}m%03d" $i;
+    printf '\e[0m';
+    [ ! $((($i - 15) % 6)) -eq 0 ] && printf ' ' || printf '\n'
+done
+
